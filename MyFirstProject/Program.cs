90691378092a339a0aa0049ff30c4d12ef540438
MahalakshmiMusanalli-gep <140727497+MahalakshmiMusanalli-gep@users.noreply.github.com>
2025-01-05 16:09:39 +0530

    ++

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-/*namespace HelloWorld
+namespace HelloWorld
 {
     public class Program
     {
@@ -16,7 +16,7 @@ using System.Threading.Tasks;
             Console.ReadLine();
         }
     }
-}*/
+}
 //Numeric datatypes
 /*namespace DataTypes
 {
